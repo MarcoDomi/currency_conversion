f98af8352b1d65501c5dd21226dc14a8efd428ae
Marco Dominguez <marcod082@gmail.com>
2024-02-15 17:25:31 -0600

    included the api calls in calculation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import currencyapicom
 import sys
 
+
 def print_values(base_pair, converted_values):
-    print(f"\nBASE CURRENCY\n{base_pair[0]}: ${base_pair[1]}")
-    print("-"*5)
+    print(f"\nBASE CURRENCY\n{base_pair[0]}: {base_pair[1]}")
+    print("-" * 5)
 
     for code in converted_values:
         print(f"{code}: {converted_values[code]}")
@@ -12,19 +13,20 @@ def print_values(base_pair, converted_values):
 def convert(base_value, exchange_rate):
     return base_value * exchange_rate
 
-# client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDics")
-# result = client.latest(base_curr, other_curr)
 
-curr = {"EUR": 0.93, "CAD": 1.35}
+client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDics")
 
 base_curr = sys.argv[1]
 other_curr = sys.argv[2:]
 base_value = float(input(f"Enter a {base_curr} amount: "))
 
+result = client.latest(base_curr, other_curr)
+other_curr = result["data"]
+
 converted_values = {}
-for code in other_curr:
-    other_val = curr[code]
-    converted_values[code] = convert(base_value, other_val)
+for key in other_curr:
+    other_val = other_curr[key]["value"]
+    converted_values[key] = convert(base_value, other_val)
 
 base_pair = (base_curr, base_value)
 print_values(base_pair, converted_values)
