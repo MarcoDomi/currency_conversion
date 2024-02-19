c29054cedef5e53f681ebb5ae8ad6cf33f479faa
Marco Dominguez <marcod082@gmail.com>
2024-02-18 18:24:48 -0600

    added fix input function

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import currencyapicom
 import sys
 
 
+def fix_input(num: float) -> str:
+    s = str(num)
+
+    digit_list = s.split(".")
+    digits_after_decimal = digit_list[1]
+
+    if len(digits_after_decimal) == 1:
+        digit_list[1] += "0"
+    elif len(digits_after_decimal) > 2:
+        digit_list[1] = digits_after_decimal[:2]
+
+    return ".".join(digit_list)
+
+
 def print_values(base_pair, converted_values):
     print(f"\nBASE CURRENCY\n{base_pair[0]}: {base_pair[1]}")
     print("-" * 5)
@@ -18,7 +32,7 @@ client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDic
 
 base_curr = sys.argv[1]
 other_curr = sys.argv[2:]
-base_value = float(input(f"Enter a {base_curr} amount: "))
+base_value = float(fix_input(input(f"Enter a {base_curr} amount: ")))
 
 result = client.latest(base_curr, other_curr)
 other_curr = result["data"]
