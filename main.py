3f50651c75d61515703021f3cc284b24cbe9aff9
Marco Dominguez <marcod082@gmail.com>
2024-02-23 15:45:18 -0600

    mend

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ POSSIBLE INCORRECT INPUTS
 12.3
 12.34345464876
 '''
-def fix_input(num: float) -> str:
+def fix_input(num: float) -> float:
     s = str(num)
-    
+
     digit_list = s.split(".")
     digits_after_decimal = digit_list[1]
 
@@ -18,14 +18,15 @@ def fix_input(num: float) -> str:
     elif len(digits_after_decimal) > 2:
         digit_list[1] = digits_after_decimal[:2]
 
-    return ".".join(digit_list)
+    correct_currency_amount = float(".".join(digit_list))
+    return correct_currency_amount
 
 
 def print_values(base_currency_pair, converted_values):
     base_currency_code = base_currency_pair[0] #base currency code
     base_currency_amount = fix_input(base_currency_pair[1]) #base currency amount
 
-    print(f"\nBASE CURRENCY\n{base_currency_code}: {base_currency_amount}\n") #why not return error?
+    print(f"\nBASE CURRENCY\n{base_currency_code}: {base_currency_amount}") 
     print("-" * 5)
     for code, value in converted_values.items():
         print(f"{code}: {value}")
@@ -35,12 +36,11 @@ def convert(base_value, exchange_rate):
     return float(base_value) * exchange_rate
 
 
-
 client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDics")
 
 base_currency_code = sys.argv[1]
 other_currency_code = sys.argv[2:]
-base_value = fix_input(float(input(f"Enter a {base_currency_code} amount: ")))
+base_value = fix_input(input(f"Enter a {base_currency_code} amount: "))
 
 result = client.latest(base_currency_code, other_currency_code)
 other_currency_data = result["data"]
