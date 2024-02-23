3462f6cd06e845f00b285f3b423a56696d2e1549
Marco Dominguez <marcod082@gmail.com>
2024-02-23 16:17:00 -0600

    extracted 'if statement' from fix_input into separate function

--- main.py
+++ main.py
@@ -7,24 +7,29 @@ POSSIBLE INCORRECT INPUTS
 12.3
 12.34345464876
 '''
-def fix_input(num: float) -> float:
+def check_digit_length(digits_str):
+    digit_count = len(digits_str)
+
+    if digit_count == 1:
+        digits_str += "0"
+    elif digit_count > 2:
+        digits_str = digits_str[:2] 
+
+    return digits_str
+
+def fix_input(num: float) -> str:
     s = str(num)
 
     digit_list = s.split(".")
     digits_after_decimal = digit_list[1]
 
-    if len(digits_after_decimal) == 1:
-        digit_list[1] += "0"
-    elif len(digits_after_decimal) > 2:
-        digit_list[1] = digits_after_decimal[:2]
+    digit_list[1] = check_digit_length(digits_after_decimal)
 
-    correct_currency_amount = float(".".join(digit_list))
+    correct_currency_amount = ".".join(digit_list)
     return correct_currency_amount
 
 
-def print_values(base_currency_pair, converted_values):
-    base_currency_code = base_currency_pair[0] #base currency code
-    base_currency_amount = fix_input(base_currency_pair[1]) #base currency amount
+def print_values(base_currency_code, base_currency_amount, converted_values):
 
     print(f"\nBASE CURRENCY\n{base_currency_code}: {base_currency_amount}") 
     print("-" * 5)
@@ -40,15 +45,15 @@ client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDic
 
 base_currency_code = sys.argv[1]
 other_currency_code = sys.argv[2:]
-base_value = fix_input(input(f"Enter a {base_currency_code} amount: "))
+base_value = fix_input(float(input(f"Enter a {base_currency_code} amount: ")))
 
 result = client.latest(base_currency_code, other_currency_code)
 other_currency_data = result["data"]
 
 converted_values = {}
 for key in other_currency_data:
-    other_val = other_currency_data[key]["value"]
-    converted_values[key] = convert(base_value, other_val)
+    other_value = other_currency_data[key]["value"]
+    converted_values[key] = convert(base_value, other_value)
 
-base_pair = (base_currency_code, base_value)
-print_values(base_pair, converted_values)
+
+print_values(base_currency_code, base_value, converted_values)
