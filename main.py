050ef8f5a8f6f25aa3a1c04959e008a0596abc70
Marco Dominguez <marcod082@gmail.com>
2024-02-27 15:46:24 -0600

    added comments

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ POSSIBLE INCORRECT INPUTS
 12.3
 12.34345464876
 '''
-
-def check_digit_length(digit_str:str) -> str:
+def append_zero(digit_str:str) -> str:
+    '''append a zero to digit string if needed'''
     digit_list = digit_str.split('.')
     digits_after_decimal = digit_list[1]
 
@@ -18,24 +18,28 @@ def check_digit_length(digit_str:str) -> str:
     return digit_str
 
 
+#there is no reason to check if number of digits after decimal is 1 for input b/c it will be converted to a float afterwards which will remove appended 0
 def format_input(digit_str: str) -> float:
-
-    if digit_str.find(".") == -1:
-        correct_currency_amount = digit_str + ".0"
-    else:
-        digit_list = digit_str.split(".")
+    '''ensure currency amount inputted is correctly formatted'''
+    if digit_str.find(".") == -1:#if no decimal point found in input 
+        correct_currency_amount = digit_str + ".0" #append .0  to string
+    else: 
+        #if no decimal was found there is no reason to execute commands under else statement
+        #split digit string by decimal and store the digits after the decimal
+        digit_list = digit_str.split(".") 
         digits_after_decimal = digit_list[1]
 
+        #if digits count after decimal is greater than 2 use slicing to remove extra digits and store in digit list
         if len(digits_after_decimal) > 2:
             digit_list[1] = digits_after_decimal[:2]
 
-        correct_currency_amount = ".".join(digit_list)
-        
-    return float(correct_currency_amount)
+        correct_currency_amount = ".".join(digit_list) #rejoin digit list using a decimal point
+
+    return float(correct_currency_amount) #convert to float and return
 
 
 def print_values(base_currency_code, base_currency_amount, converted_values):
-    
+    '''print all info related to currency conversion'''
     print(f"\nBASE CURRENCY\n{base_currency_code}: {base_currency_amount}") 
     print("-" * 5)
     for code, value in converted_values.items():
@@ -43,22 +47,24 @@ def print_values(base_currency_code, base_currency_amount, converted_values):
 
 
 def convert(base_value:float, exchange_rate:float):
-    return base_value * exchange_rate
+    '''perform currency conversion by multiplying base value with exchange rate'''
+    return base_value * exchange_rate 
 
 
 client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDics")
 
-base_currency_code = sys.argv[1]
-other_currency_code = sys.argv[2:]
-base_value = format_input(input(f"Enter a {base_currency_code} amount: "))
+base_currency_code = sys.argv[1]   #first command line arg is the base currency
+other_currency_code = sys.argv[2:] #all following command line args are currencies the base currency will be converted to
+base_value = format_input(input(f"Enter a {base_currency_code} amount: ")) #enter currency amount and ensure the amount is formatted correctly
 
-result = client.latest(base_currency_code, other_currency_code)
-other_currency_data = result["data"]
+result = client.latest(base_currency_code, other_currency_code)#use api to obtain latest currency exchange rates of 'other currency' relative to base currency
+other_currency_data = result["data"]  #store data about the other currency codes
 
 converted_values = {}
-for key in other_currency_data:
-    other_value = other_currency_data[key]["value"]
-    converted_values[key] = convert(base_value, other_value)
+for key in other_currency_data: #iterate thru other currencies
+    other_value = other_currency_data[key]["value"] #obtain exchange rate relative to base currency for current 'other currency'
+    converted_values[key] = convert(base_value, other_value)#perform conversion
 
-base_value = check_digit_length(str(base_value))
+base_value = append_zero(str(base_value)) #check length of digits after decimal. 
+                                          #its guaranteed the num of digits after decimal will not exceed 2
 print_values(base_currency_code, base_value, converted_values)
