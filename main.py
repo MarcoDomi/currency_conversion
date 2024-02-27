9abb4d529626584bb801d845eb6340167df4a997
Marco Dominguez <marcod082@gmail.com>
2024-02-27 15:57:48 -0600

    created new function get_converted_values

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def append_zero(digit_str:str) -> str:
     return digit_str
 
 
-#there is no reason to check if number of digits after decimal is 1 for input b/c it will be converted to a float afterwards which will remove appended 0
+# there is no reason to check if number of digits after decimal is 1 for input b/c it will be converted to a float afterwards which will remove appended 0
 def format_input(digit_str: str) -> float:
     '''ensure currency amount inputted is correctly formatted'''
     if digit_str.find(".") == -1:#if no decimal point found in input 
@@ -51,6 +51,16 @@ def convert(base_value:float, exchange_rate:float):
     return base_value * exchange_rate 
 
 
+def get_converted_values(base_amount, currency_data):
+    '''get dictionary of converted base values for each desired currency'''
+    converted_values = {}
+    for key in currency_data:  #iterate thru other currencies
+        other_value = currency_data[key]["value"]  #obtain exchange rate relative to base currency for current 'other currency'
+        converted_values[key] = convert(base_amount, other_value)  #perform conversion
+
+    return converted_values
+
+
 client = currencyapicom.Client("cur_live_5X7DY8sMMB7D1OCA741glYl7mrdiCy9sPz1VDics")
 
 base_currency_code = sys.argv[1]   #first command line arg is the base currency
@@ -60,11 +70,8 @@ base_value = format_input(input(f"Enter a {base_currency_code} amount: ")) #ente
 result = client.latest(base_currency_code, other_currency_code)#use api to obtain latest currency exchange rates of 'other currency' relative to base currency
 other_currency_data = result["data"]  #store data about the other currency codes
 
-converted_values = {}
-for key in other_currency_data: #iterate thru other currencies
-    other_value = other_currency_data[key]["value"] #obtain exchange rate relative to base currency for current 'other currency'
-    converted_values[key] = convert(base_value, other_value)#perform conversion
+converted_values = get_converted_values(base_value, other_currency_data)
 
-base_value = append_zero(str(base_value)) #check length of digits after decimal. 
-                                          #its guaranteed the num of digits after decimal will not exceed 2
+# it is guaranteed the num of digits after decimal will not exceed 2 by this point
+base_value = append_zero(str(base_value)) 
 print_values(base_currency_code, base_value, converted_values)
